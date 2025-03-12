cab76027e653901165bf28ebbfc237dab9f51c35
Neetu gupta <100510822+neetuAmritsar@users.noreply.github.com>
2025-03-12 10:56:04 +0530

    Initial commit for Vite project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+ import Header from './components/Header'
+ import Home from './pages/Home'
+import './App.css'
+
+function App() {
+   
+  return (
+    <>
+       <Header/>
+      <Home/>
+       
+     
+    </>
+  )
+}
+
+export default App
